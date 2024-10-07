36684934a7051a2613bcf077f15606349669c34e
stellviaanime <stellviaanime@gmail.com>
2024-10-07 10:38:01 -0400

    upgrade build.sh on export folder

--- export/build.sh
+++ export/build.sh
@@ -1,3 +1,3 @@
-#GOOS=windows GOARCH=amd64 go build -o ../pyfract/fract.dll -buildmode=c-shared .
-GOOS=linux GOARCH=amd64 go build -o ../pyfract/fract.so -buildmode=c-shared .
+GOOS=windows GOARCH=amd64 go build -o ../pyfract/fract.dll -buildmode=c-shared -ldflags="-s -w" .
+#GOOS=linux GOARCH=amd64 go build -o ../pyfract/fract.so -buildmode=c-shared .
 #CC=x86_64-w64-mingw32-gcc GOOS=linux GOARCH=amd64 go build -o ../pyfract/fract.so -buildmode=c-shared .
